21339fec7674418790b66f3edf0df462cd76166d
Ismail Magomedov <caspian.seagull@gmail.com>
2020-01-14 16:29:32 +0300

    Fix

--- src/pages/demos/css-filter-goo/index.tsx
+++ src/pages/demos/css-filter-goo/index.tsx
@@ -1,10 +1,31 @@
 import React, { useState } from 'react'
-import { Layout$, Cell$, Navigation$ } from './styles'
+
+import styled from 'styled-components'
+
+export const Layout$ = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Cell$ = styled.div`
+  width: 480px;
+  height: 480px;
+`
+
+export const Navigation$ = styled.nav`
+  width: 480px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
 
 import GooDemo1 from 'components/demos/css-goo/demo1'
 import GooDemo2 from 'components/demos/css-goo/demo2'
-
-
 
 const CssGoo = () => {
   const [currentDemo, setCurrentDemo] = useState(0)
