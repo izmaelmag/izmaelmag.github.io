0c927f421f2c049a0403d949d228978c5ca85460
Ismail Magomedov <caspian.seagull@gmail.com>
2020-04-14 03:57:11 +0300

    Smol ficks

--- src/pages/ui-kit.tsx
+++ src/pages/ui-kit.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import CommonLayout from 'layouts/Common'
 import ToggleKit from 'components/Toggle/kit'
@@ -9,7 +9,7 @@ const UiKit = () => {
     <CommonLayout>
       <Page$>
         <ToggleKit />
-      </Page$>
+      </Page$> 
     </CommonLayout>
   )
 }
