42bfacd9739f80c1245bc431347e8dc73c3c696a
str <str@Seagull.local>
2020-05-31 22:37:23 +0300

    Perlin noise test sketch

---
+++ src/sketches/noise/sketchSettings.ts
@@ -0,0 +1,14 @@
+const sketchSettings: ISettingsList = {
+    radius: {
+      type: 'range',
+      value: 50,
+      props: {
+        min: 10,
+        max: 200,
+        step: 1
+      },
+      title: 'Radius'
+    }
+  }
+  
+  export default sketchSettings
