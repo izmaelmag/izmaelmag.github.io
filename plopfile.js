1f0bab4d7dc08bac8f85c7961b3668a3a0227757
Ismail Magomedov <ismail@variant.net>
2023-01-05 03:20:29 +0900

    Config updates

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,4 @@
-export default function plopGenerator(plop) {
+module.exports = function plopGenerator(plop) {
   plop.setGenerator("component", {
     description: "Creates new react component",
     prompts: [
@@ -11,19 +11,22 @@ export default function plopGenerator(plop) {
     actions: [
       {
         type: "add",
+        skipIfExists: true,
         path: "components/{{ pascalCase name }}/index.ts",
         templateFile: "config/plop/component/index.ts.hbs",
       },
       {
         type: "add",
+        skipIfExists: true,
         path: "components/{{ pascalCase name }}/styles.module.css",
         templateFile: "config/plop/component/styles.module.css",
       },
       {
         type: "add",
+        skipIfExists: true,
         path: "components/{{ pascalCase name }}/{{ pascalCase name }}.tsx",
         templateFile: "config/plop/component/component.tsx.hbs",
       },
     ],
   });
-}
+};
