1aaa159ab22f6c92327270f07bd1b81a559e5c70
Ismail Magomedov <ismail@variant.net>
2023-01-31 22:16:09 +0900

    Fixed paintWOrklet check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import "../styles/animations.css";
 import type { AppProps } from "next/app";
 
-if ("paintWorklet" in CSS) {
+if (typeof CSS !== "undefined" && "paintWorklet" in CSS) {
   // @ts-ignore
   CSS.paintWorklet.addModule(
     "https://www.unpkg.com/css-houdini-squircle@0.1.3/squircle.min.js"
